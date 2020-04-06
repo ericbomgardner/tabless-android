5e4895a5e0d9cbd9d5edeca22a6a539615d6f2e5
Eric Bomgardner <eric.bomgardner@gmail.com>
2020-04-05 21:26:43 -0700

    Very barebones working version

--- app/src/main/java/app/tabless/MainActivity.kt
+++ app/src/main/java/app/tabless/MainActivity.kt
@@ -1,12 +1,69 @@
 package app.tabless
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var searchField: EditText? = null
+    private var webView: WebView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchField = findViewById<EditText>(R.id.search_field)
+        this.searchField = searchField
+        searchField.setOnEditorActionListener { v: TextView?, actionId: Int, event: KeyEvent? ->  
+            if (event?.action == KeyEvent.ACTION_DOWN) {
+                loadSearch()
+                return@setOnEditorActionListener true
+            }
+            false
+        }
+
+        val webView = findViewById<WebView>(R.id.web_view)
+        this.webView = webView
+
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+    }
+
+    override fun onBackPressed() {
+        if (webView?.canGoBack() == true) {
+            webView?.goBack()
+        } else if (webView?.visibility == View.VISIBLE) {
+            webView?.visibility = View.INVISIBLE
+            webView?.stopLoading()
+            webView?.clearHistory()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    private fun loadSearch() {
+        val searchText = searchField?.text.toString().takeIf { it.length > 0 } ?: return
+
+        // Hide keyboard
+        val inputMethodManager =
+            applicationContext.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(searchField?.windowToken, 0)
+
+        // Show and prepare web view
+        webView?.visibility = View.VISIBLE
+        val urlString = URLBuilder().createURLString(searchText)
+        webView?.loadUrl(urlString)
+
+        // Clear search field's text
+        searchField?.text?.clear()
     }
 }
---
+++ app/src/main/java/app/tabless/URLBuilder.kt
@@ -0,0 +1,47 @@
+package app.tabless
+
+import java.net.URLEncoder
+
+class URLBuilder {
+    private val searchURLBase = "https://www.google.com/search?q="
+
+    /// Whether input text should be treated as a web URL
+    fun shouldTreatAsWebURL(text: String): Boolean {
+        return canTreatAsWebURL(text) && text.contains(".")
+    }
+
+    /// If text looks like a URL, creates that URL
+    /// Otherwise, treats it like a search query and returns a search URL
+    fun createURLString(text: String): String? {
+        return if (shouldTreatAsWebURL(text)) {
+            createWebURL(text)
+        } else {
+            createSearchURL(text)
+        }
+    }
+
+    private fun createWebURL(text: String): String? {
+        return asWebURL(text)
+    }
+
+    /// Whether a valid web URL can be created from the string
+    private fun canTreatAsWebURL(text: String): Boolean {
+        return asWebURL(text) != null
+    }
+
+    /// Representation of the string as a web URL
+    private fun asWebURL(text: String): String? {
+        val urlString: String
+        if (!text.startsWith("http")) {
+            urlString = "https://${text}"
+        } else {
+            urlString = text
+        }
+        return urlString
+    }
+
+    private fun createSearchURL(text: String): String? {
+        val searchQuery = URLEncoder.encode(text, "UTF-8")
+        return "${searchURLBase}${searchQuery}"
+    }
+}
